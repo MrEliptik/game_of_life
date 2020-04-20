c08f3e2c0dec4996d7b653f1241c662c32023431
Victor MEUNIER <vmeunier@signoptic.local>
2020-04-20 11:41:52 +0200

    change color and random probabilities of live cell and the beginning

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const TIMEOUT = 100;
 const LIVE_P_MAX = 0.5;
-const LIVE_P_MIN = 0.05;
+const LIVE_P_MIN = 0.01;
 
 var rows = 50;
 var cols = 50;
@@ -19,6 +19,14 @@ var grid = Array.from(Array(rows), _ =>
     Array(cols).fill(0)
 );
 
+canvas.addEventListener("touchend", handleEnd, false);
+
+start();
+
+function handleEnd() {
+    start();
+}
+
 function drawGrid(color="#cccccc") {
     ctx.beginPath();
 
@@ -36,6 +44,26 @@ function drawGrid(color="#cccccc") {
     ctx.stroke();
 }
 
+function rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch(i % 6){
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
+    }
+    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    return (c);
+}
 
 function drawContent(content){
     for (var i = 0; i < rows; i++) {
@@ -122,14 +150,13 @@ function loop(){
     console.log('tick');
     update();
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    drawGrid();
+    //drawGrid();
     drawContent(grid);
     setTimeout(loop, TIMEOUT);
 }
 
-// Random number of cell alive
-var cellsAlive = Math.floor(Math.random() * ((rows*cols) - 1) + 1);
-
-randomSeed()
-
-loop();
+function start(){
+    ctx.fillStyle = rainbow(256, Math.floor(Math.random() * 256));
+    randomSeed();
+    loop();
+}
